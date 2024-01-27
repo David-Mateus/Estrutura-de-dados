9946895fc125023d4ef27dce233a78939a947336
David-Mateus <david-mateus09@hotmail.com>
2024-01-27 14:19:01 -0300

    mergeAlternately

--- Pesquisa_Binaria/Array_String/mergeStringsAlternately.py
+++ Pesquisa_Binaria/Array_String/mergeStringsAlternately.py
@@ -1,9 +1,16 @@
-str1 = 'abc'
-str2 = 'pqr'
-resultado = ''
-tamanho = min(len(str1), len(str2))
-for i in range(tamanho):
-    resultado += str1[i] + str2[i]
-print(resultado)
+
+class Solution():
+     def mergeAlternately(self, word1: str, word2: str) -> str:
+          result = ''
+          lenght = min(len(word1), len(word2))
+          for i in range(lenght):
+               result += word1[i] + word2[i]
+          result += word1[lenght:] + word2[lenght:]
+          return result
+        
+
+words = Solution()
+print(words.mergeAlternately("abc", "pqr"))
+               
 
     
